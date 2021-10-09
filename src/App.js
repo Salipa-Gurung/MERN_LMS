bc2347b39f7cf4e1d96d0ace6469e45433a10663
Salipa-Gurung <salipagurung@gmail.com>
2021-10-09 16:47:51 +0545

    use axios to call server API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+const [data,setData] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/test')
+      .then((res)=>{
+        setData(res.data.data)
+      })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.map((person)=>(
+          <h1 key={person.id}>{person.name}</h1>
+        ))
+      }
     </div>
   );
 }
