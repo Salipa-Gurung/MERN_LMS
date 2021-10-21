d606715746ee8a6acd23862fba5ab14ba6b6668b
Salipa-Gurung <salipagurung@gmail.com>
2021-10-21 16:48:34 +0545

    add sidebar and css module
    1.sidebar added
    2.add module.css for app.js and sidebar
    3.navbar breakdown into separate component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 
-import axios from 'axios';
-
+import styles from './App.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import BookList from './components/books/bookList';
@@ -13,56 +11,49 @@ import SupplierDetail from './components/suppliers/supplierDetail';
 import SupplierList from './components/suppliers/supplierList'
 import AddSupplier from './components/suppliers/addSupplier'
 import EditSupplier from './components/suppliers/editSupplier'
+import Navbar from './components/navbar/navbar';
+import Sidebar from './components/sidebar/sidebar';
 
 // top: react imports -> third-party import -> css import/styled components -> last: our own module 
 
 function App() {
-// const [data,setData] = useState([])
-
-  useEffect(()=>{
-    // axios.get('http://localhost:5000/test')
-    //   .then((res)=>{
-    //     setData(res.data.data)
-    //   })
-  },[])
 
   return (
     <Router>
-      <header className="navbar bg-light p-3">
-        <span>NavBar</span>
-        <button className="btn btn-danger">Log out</button>
-      </header>
-      <main className="p-5">
-        <Switch>
-          <Route path="/book" exact>
-            <BookList/>
-          </Route>
-          <Route path="/book/create">
-            <CreateBook/>
-          </Route>
-          <Route path="/book/edit/:id">
-            <EditBook/>
-          </Route>
-          <Route path="/book/:id">
-            <BookDetail/>
-          </Route>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <div className={styles.main}>
 
-          <Route path="/suppliers" exact>
-            <SupplierList/>
-          </Route>
-          <Route path="/suppliers/create">
-            <AddSupplier/>
-          </Route>
-          <Route path="/supplier/edit/:id">
-            <EditSupplier/>
-          </Route>
-          <Route path="/supplier/:id">
-            <SupplierDetail/>
-          </Route>
+          <Navbar/>
+            <Switch>
+              <Route path="/book" exact>
+                <BookList/>
+              </Route>
+              <Route path="/book/create">
+                <CreateBook/>
+              </Route>
+              <Route path="/book/edit/:id">
+                <EditBook/>
+              </Route>
+              <Route path="/book/:id">
+                <BookDetail/>
+              </Route>
 
-        </Switch>
-      </main>
-
+              <Route path="/suppliers" exact>
+                <SupplierList/>
+              </Route>
+              <Route path="/suppliers/create">
+                <AddSupplier/>
+              </Route>
+              <Route path="/supplier/edit/:id">
+                <EditSupplier/>
+              </Route>
+              <Route path="/supplier/:id">
+                <SupplierDetail/>
+              </Route>
+            </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className="navbar bg-light p-3">
+            <span>NavBar</span>
+            <button className="btn btn-danger">Log out</button>
+        </header>
+    )
+}
+
+export default Navbar
